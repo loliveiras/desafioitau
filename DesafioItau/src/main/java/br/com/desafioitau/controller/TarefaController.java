95270ee4a1b30719101622ae225f06dfb8e300d9
Leandro <leandro_oliveira_silva@yahoo.com.br>
2019-09-30 11:52:11 -0300

    Adicionando arquivos desafio itau

--- DesafioItau/src/main/java/br/com/desafioitau/controller/TarefaController.java
+++ DesafioItau/src/main/java/br/com/desafioitau/controller/TarefaController.java
@@ -3,37 +3,55 @@ package br.com.desafioitau.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.desafioitau.entity.Tarefa;
 import br.com.desafioitau.repository.TarefaRepository;
 import br.com.desafioitau.util.TarefaStatusEnum;
 
-@Component
+@RestController
+@RequestMapping("/services/tarefa")
 public class TarefaController {
 
 	@Autowired
 	private TarefaRepository repository;
 
-	public Tarefa saveTarefa(@RequestBody Tarefa tarefa) {
+	@PostMapping()
+	public ResponseEntity<String> saveTarefa(@Valid @RequestBody Tarefa tarefa){
+		if(!validateStatus(tarefa))
+			return new ResponseEntity<>("Invalid Value Status", HttpStatus.PRECONDITION_FAILED);
+		
 		repository.save(tarefa);
-		return tarefa;
+		return new ResponseEntity<>("Success Create Task", HttpStatus.OK);
 	}
 
-	public List<Tarefa> listAllTarefa() {
+	@GetMapping()	
+	public List<Tarefa> listTarefas() {
 		List<Tarefa> tarefas = repository.list();
 		return tarefas;
 	}
 
-	public Tarefa updateTarefa(Integer id, Tarefa tarefa) {
+	@PutMapping("/{id}")
+	public ResponseEntity<String> updateTarefa(@PathVariable(value = "id") Integer id, @Valid @RequestBody Tarefa tarefa) {
+		if(validateStatus(tarefa))
+			return new ResponseEntity<>("Invalid Value Status", HttpStatus.PRECONDITION_FAILED);
 		Optional<Tarefa> tarefas = repository.findById(id);
 
 		tarefas.get().setNome(tarefa.getNome());
 		tarefas.get().setStatus(tarefa.getStatus());
-		final Tarefa tarefa1 = repository.save(tarefas.get());
-		return tarefa1;
+		repository.save(tarefas.get());
+		return new ResponseEntity<>("Success Update Task", HttpStatus.OK);
 	}
 	
 	public boolean validateStatus(Tarefa tarefa) {
